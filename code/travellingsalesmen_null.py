bd384a5f534c9a1b1b304cfbed9403cde76b0391
gen <genesisargueta@dhcp-10-5-60-209.dhcp.nd.edu>
2024-10-08 23:05:10 -0400

    basic setup complete

--- code/travellingsalesmen_null.py
+++ code/travellingsalesmen_null.py
@@ -1,20 +1,44 @@
 #!/usr/bin/env python3
 
-import numpy as np
+import csv
+from itertools import permutations
 
 
 def read_cities(filepath):
-    """
-    Load a TSP dataset from a CSV file.
+  """
+  Load a TSP dataset from a CSV file located in /test.
+  tiny.csv - 10 cities
+  small.csv - 30 cities
+  medium.csv - 100 cities
+  large.csv - 1000 cities
+  the data is formated as a city per line with x,y cords
 
-    Returns:
-    cities (np.ndarray): NumPy array of city coordinates.
+  Returns:
+  cities list of lists [[x,y],[x,y]...]
 
-    """
-    
-    cities = np.loadtxt(filepath, delimiter=',')
-    return cities
+  """
+
+  cities = []
+  with open(filepath, 'r') as file:
+    for line in file:
+      x, y = line.strip().split(',')
+      cities.append([float(x), float(y)])
+  return cities
+
+def brute_force_tsm(cities):
+  """
+  here is where we will brute force the solution
+  """
+  # init out variables
+  best_path =  None # 
+  shortest_distance = float("inf")
+  indicies = list(range(1,len(cities)))  # starting at city 0
+  for p in permutations(indicies):
+    p = [0] + list(p)   # remember, we start at city 0
+    ##TODO: calc distance between the vector
 
 if __name__ == '__main__':
     cities = read_cities('../test/tiny.csv')
+    brute_force_tsm(cities)
+
 
