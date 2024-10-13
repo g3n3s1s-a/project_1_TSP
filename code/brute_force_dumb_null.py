46e1261bf123af1dea563a2ffb5b9163a85018e6
Genesis Argueta <gargueta@nd.edu>
2024-10-13 11:43:24 -0400

    tried adding graph

--- code/brute_force_dumb_null.py
+++ code/brute_force_dumb_null.py
@@ -3,6 +3,7 @@
 from itertools import permutations
 from helper_funcs_null import read_cities,score
 import numpy as np
+import matplotlib.pyplot as plt
 
   
 def brute_force_tsp(cities):
@@ -15,17 +16,37 @@ def brute_force_tsp(cities):
   indicies = np.arange(1, len(cities))  # we'll force starting at city 0
   for p in permutations(indicies):
     p = [0] + list(p)   # remember, we start at city 0
-    ##TODO: calc distance between the vector
     dist =  score(cities,p)
     if dist < shortest_distance:
         shortest_distance = dist
         best_path = p
   return best_path,shortest_distance
+
+def graph(cities,best_path,shortest_distance):
+    order = np.array(best_path) # this are the indicies of the cities
+
+    # Reorder points based on the specified order
+    ordered_points = cities[order]
+    
+    x = ordered_points[:,0]
+    y = ordered_points[:,1]
+    
+    # Create the plot
+    plt.plot(x, y, marker='o', linestyle='-', color='blue', label='Ordered Path')
+    # Add labels and title
+    plt.xlabel('X axis')
+    plt.ylabel('Y axis')
+    plt.title('Plot of Points in Custom Order')
+    plt.legend()
+
+    # Save the plot to a file
+    plt.savefig('custom_order_plot.png')
     
 
 if __name__ == '__main__':
     cities = read_cities('../test/tiny.csv')
     best_path, shortest_distance = brute_force_tsp(cities)
+    graph(cities,best_path,shortest_distance)
     print(best_path,shortest_distance)
 
 
