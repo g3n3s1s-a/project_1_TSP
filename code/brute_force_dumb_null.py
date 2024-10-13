3c0e0e9f18ee97774e866bc3665ef0a990c1c2c8
gen <genesisargueta@dhcp-10-5-60-209.dhcp.nd.edu>
2024-10-13 12:06:49 -0400

    graph

--- code/brute_force_dumb_null.py
+++ code/brute_force_dumb_null.py
@@ -5,7 +5,6 @@ from helper_funcs_null import read_cities,score
 import numpy as np
 import matplotlib.pyplot as plt
 
-  
 def brute_force_tsp(cities):
   """
   here is where we will brute force the solution
@@ -22,31 +21,35 @@ def brute_force_tsp(cities):
         best_path = p
   return best_path,shortest_distance
 
-def graph(cities,best_path,shortest_distance):
+def graph(cities,best_path,shortest_distance,data,algo,start=0):
     order = np.array(best_path) # this are the indicies of the cities
 
     # Reorder points based on the specified order
     ordered_points = cities[order]
-    
+    # Add the starting city to the end to complete the circuit
+    ordered_points = np.vstack([ordered_points, ordered_points[start]])
+
     x = ordered_points[:,0]
     y = ordered_points[:,1]
-    
-    # Create the plot
-    plt.plot(x, y, marker='o', linestyle='-', color='blue', label='Ordered Path')
-    # Add labels and title
+
+    #Create the plot
+    plt.plot(x, y, marker='o', linestyle='-', color='blue', label=f'Total Distance : {round(shortest_distance,2)}')
+    #Add labels and title
     plt.xlabel('X axis')
     plt.ylabel('Y axis')
-    plt.title('Plot of Points in Custom Order')
+    plt.title(f'{algo} : {data} path')
     plt.legend()
 
     # Save the plot to a file
-    plt.savefig('custom_order_plot.png')
-    
+    name,_ = data.split('.')
+    plt.savefig(f'{algo}_{name}_plot.png')
+
 
 if __name__ == '__main__':
-    cities = read_cities('../test/tiny.csv')
+    data = 'tiny.csv'
+    cities = read_cities(f'../test/{data}')
     best_path, shortest_distance = brute_force_tsp(cities)
-    graph(cities,best_path,shortest_distance)
+    graph(cities,best_path,shortest_distance,data,"brute force")
     print(best_path,shortest_distance)
 
 
